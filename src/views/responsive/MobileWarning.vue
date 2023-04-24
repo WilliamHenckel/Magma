4c6a5231d7bd59a2e35ae7c7044e7d25ac1d3474
William Henckel <williamhenckel@hotmail.com>
2023-04-24 11:21:37 +0200

    [main] Responsive in english

--- src/views/responsive/MobileWarning.vue
+++ src/views/responsive/MobileWarning.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="md:hidden flex flex-col items-center justify-center h-screen">
     <img src="../../assets/wall.jpg" class="w-60 mb-10" alt="wall" />
-    <p class="text-2xl">Largeur disponible insuffisante !</p>
-    <p>Merci d'utiliser un terminal d'au moins 768px de large</p>
+    <p class="text-2xl">Insufficient width !</p>
+    <p>Please use a larger device (768px minimum)</p>
   </div>
 </template>
